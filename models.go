3d43e4d8e5034b7665200afcac673ed46e376d67
Nate Robinson <nathanaeljrobinson@gmail.com>
2023-11-21 20:31:31 -0800

    renamed sockepath

--- models.go
+++ models.go
@@ -252,7 +252,7 @@ func (a *App) InitializeCloud(user, password, database, connection string) {
 			Colorful:                  true,        // Disable color
 		},
 	)
-	socketPath := "/cloudsql/" + connection + "/.s.PGSQL.5432"
+	socketPath := "/cloudsql/snowpack-368423:us-central1:cronos"
 	dbURI := fmt.Sprintf("user=%s password=%s database=%s host=%s", user, password, database, socketPath)
 	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{
 		Logger:                                   newLogger,
